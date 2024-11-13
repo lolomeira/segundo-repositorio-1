4f23ac9c45fe2d8c6fb4991d37c93e2c2c25d99d
jasma526 <160799355+jasma526@users.noreply.github.com>
2024-11-12 21:51:08 -0300

    teste 2

---
+++ missaovilaovalores.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Missão',
+    'Saiba qual é a missão de nossa empresa!',
+    'Nossa missão é  trazer alegria para a pessoas atráves de deliciosas trufas.'
+)
+
+criaCartao (
+    'Visão',
+    'Saiba qual é a visão de nossa empresa!',
+    'Nossa visão é Ser uma confeiteira reconhecida pelas pessoas e pelo sabor que reelembra a infancia'
+)
+
+criaCartao (
+    'Valores',
+    'Saiba quais são os valores de nossa empresa!',
+    'Nossos valores são Qualidade, sustentabilidade, inovação, transparencia, satisfação ao cliente.'
+)
